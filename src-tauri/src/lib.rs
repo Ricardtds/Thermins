0869f4d85234399efccad9c1b85aa41ac6408b46
Ricardo Teixeira dos Santos <ricardoteixeirasantos@hotmail.com>
2026-05-27 15:47:16 -0300

    feat: :sparkles: Sending system_info via invoker command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,18 +3,14 @@ mod models;
 mod telemetry;
 
 use telemetry::emitter::start_telemetry;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use collector::system::get_system_info;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet
+            get_system_info
         ])
         .setup(|app| {
             start_telemetry(app.handle().clone());
